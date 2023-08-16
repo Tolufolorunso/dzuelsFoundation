a486cdd8a266fd4dbeda6dedf1da8a142e3cc5d3
tolufolorunso <tolufolorunso@yahoo.com>
2023-08-16 16:35:34 +0100

    changes made

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -1,10 +1,18 @@
+import Container from '@/components/layout/container'
+import Link from 'next/link'
+
 function Error({ statusCode }) {
   return (
-    <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : 'An error occurred on client'}
-    </p>
+    <Container>
+      <p>
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : 'An error occurred on client'}
+      </p>
+      <p>
+        Go <Link href='/'>back Home</Link>
+      </p>
+    </Container>
   )
 }
 
