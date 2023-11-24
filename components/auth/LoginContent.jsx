55b69d6e280d66ba94765988125b1ddf6c47d8cf
toluFolorunso <tolufolorunso@yahoo.com>
2023-11-24 12:55:22 +0100

    updated

--- components/auth/LoginContent.jsx
+++ components/auth/LoginContent.jsx
@@ -17,6 +17,7 @@ function LoginContent(props) {
   })
 
   function handleChenge(event) {
+    event.preventDefault()
     setState({ ...state, [event.target.name]: event.target.value })
   }
 
@@ -28,35 +29,35 @@ function LoginContent(props) {
   return (
     <Box className={classes.container}>
       <Paper className={classes.paper}>
-        <Typography variant='h5' sx={{ mb: 2 }}>
+        <Typography variant="h5" sx={{ mb: 2 }}>
           Login
         </Typography>
         <TextField
-          label='Username'
-          variant='outlined'
-          margin='normal'
+          label="Username"
+          variant="outlined"
+          margin="normal"
           fullWidth
           onChange={handleChenge}
-          name='username'
+          name="username"
         />
         <TextField
-          label='Password'
-          variant='outlined'
-          margin='normal'
-          type='password'
+          label="Password"
+          variant="outlined"
+          margin="normal"
+          type="password"
           fullWidth
           onChange={handleChenge}
-          name='password'
+          name="password"
         />
         <Button
-          variant='contained'
-          color='primary'
+          variant="contained"
+          color="primary"
           sx={{ mt: 2 }}
           onClick={clickHandler}
         >
           {loading ? (
             <>
-              <CircularProgress size={10} color='inherit' />
+              <CircularProgress size={10} color="inherit" />
               <span style={{ marginLeft: '5px' }}>Login...</span>
             </>
           ) : (
@@ -64,8 +65,8 @@ function LoginContent(props) {
           )}
         </Button>
 
-        <Link href='/auth/register' passHref>
-          <Button variant='text' color='primary' sx={{ mt: 2 }}>
+        <Link href="/auth/register" passHref>
+          <Button variant="text" color="primary" sx={{ mt: 2 }}>
             Register instead?
           </Button>
         </Link>
