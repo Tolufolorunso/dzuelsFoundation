effdc999f241b40ecc040f1d0e1308bf1daf7d58
toluFolorunso <tolufolorunso@yahoo.com>
2023-12-01 09:48:14 +0100

    updated

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -41,7 +41,7 @@ function MyApp(props) {
     currentDate.getDate() <= 25
 
   function renderChristmasMessage() {
-    if (true) {
+    if (false) {
       return (
         <div
           style={{
@@ -51,7 +51,7 @@ function MyApp(props) {
             padding: '10px',
             marginTop: '-10px',
             position: 'fixed',
-            width: '100%',
+            // width: '100%',
           }}
         >
           It is Christmas time! 🎅🎄 Spread joy and happiness!
@@ -60,6 +60,54 @@ function MyApp(props) {
     }
     return null
   }
+
+  React.useEffect(() => {
+    const colors = ['rgb(182, 7, 10)', 'rgb(84, 227, 158)', 'rgb(255, 99, 71)']
+    const animations = ['fall-1', 'fall-2', 'fall-3']
+
+    const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+    const selectRandom = (items) => items[rand(0, items.length - 1)]
+
+    const originPosition = { x: 0, y: 0 }
+
+    const last = {
+      starPosition: originPosition,
+      mousePosition: originPosition,
+    }
+
+    const calcDistance = (a, b) => {
+      const diffX = b.x - a.x
+      const diffY = b.y - a.y
+      return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2))
+    }
+
+    window.onmousemove = (e) => {
+      const mouseX = e.clientX
+      const mouseY = e.clientY
+      const star = document.createElement('span')
+      star.className = 'star fa-star fa-solid'
+      star.style.left = `${mouseX}px`
+      star.style.top = `${mouseY}px`
+      star.style.color = selectRandom(colors)
+      star.style.animationName = selectRandom(animations)
+
+      const glow = document.createElement('div')
+      glow.className = 'glow-point'
+      glow.style.left = `${mouseX}px`
+      glow.style.top = `${mouseY}px`
+
+      last.mousePosition = { x: e.clientX, y: e.clientY }
+
+      document.body.appendChild(glow)
+      setTimeout(() => document.body.removeChild(glow), 75)
+
+      if (calcDistance(last.starPosition, last.mousePosition) >= 100) {
+        document.body.appendChild(star)
+        last.starPosition = last.mousePosition
+        setTimeout(() => document.body.removeChild(star), 1500)
+      }
+    }
+  }, [])
 
   return (
     <SessionProvider session={pageProps.session}>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,50 +3,57 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head>
           {/* <link rel='shortcut icon' href='/favicon.ico' /> */}
 
-          <link rel='icon' href='/favicon.ico' />
+          <link rel="icon" href="/favicon.ico" />
           <link
-            rel='icon'
-            type='image/png'
-            sizes='32x32'
-            href='/favicon-32x32.png'
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
           />
           <link
-            rel='icon'
-            type='image/png'
-            sizes='16x16'
-            href='/favicon-16x16.png'
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
           />
-          <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <link
-            rel='icon'
-            type='image/png'
-            sizes='192x192'
-            href='/android-chrome-192x192.png'
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/android-chrome-192x192.png"
           />
           <link
-            rel='icon'
-            type='image/png'
-            sizes='512x512'
-            href='/android-chrome-512x512.png'
+            rel="icon"
+            type="image/png"
+            sizes="512x512"
+            href="/android-chrome-512x512.png"
           />
 
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            rel='preconnect'
-            href='https://fonts.gstatic.com'
-            crossOrigin='true'
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
           />
           <link
-            href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;0,900;1,400&display=swap'
-            rel='stylesheet'
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+            integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;0,900;1,400&display=swap"
+            rel="stylesheet"
           ></link>
           <link
-            rel='stylesheet'
-            href='https://fonts.googleapis.com/icon?family=Material+Icons'
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
         </Head>
         <body>
